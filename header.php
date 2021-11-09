92322a9206839d2d409e43651d9c4e468357d4ea
MoonKabir <moonkabir4@gmail.com>
2021-11-09 23:48:39 +0600

    body class added in the header.php file

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Header template.
  *
@@ -7,11 +8,14 @@
 ?>
 <!doctype html>
 <html lang="<?php language_attributes(); ?>">
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<?php wp_head(); ?>
 </head>
-<body>
-<header>Header</header>
+
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<header>Header</header>

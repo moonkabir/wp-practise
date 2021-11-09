f20cf8cc1ebd6cc94c1b21b66da82e63085e6be0
MoonKabir <moonkabir4@gmail.com>
2021-11-09 23:38:40 +0600

    after theme setup hook we can pass the title tag

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@
  *
  * @package Practise
  */
+
+ /**
+ * Essential theme supports
+ */
+function theme_setup(){
+	/** tag-title **/
+	add_theme_support( 'title-tag' );
+}
+add_action('after_setup_theme','theme_setup');

a5a11fed8c2614b451ae289f2c1404973ba69859
MoonKabir <moonkabir4@gmail.com>
2021-11-20 21:48:55 +0600

    handling title tag

--- inc/classes/class-practise-theme.php
+++ inc/classes/class-practise-theme.php
@@ -13,6 +13,7 @@ class PRACTISE_THEME{
 
     protected function __construct(){
         // load Class
+        assets::get_instance();
         $this->setup_hooks();
     }
 
@@ -21,7 +22,10 @@ class PRACTISE_THEME{
         * Actions
         */
 
-        assets::get_instance();
+        add_action('after_setup_theme', [ $this, 'setup_theme'] );
     }
 
+    public function setup_theme(){
+        add_theme_support('title-tag');
+    }
 }
